266bb6c577c791b6aebd2140219e5dc4e402e1c5
--global <ageorgiou@noa.gr>
2025-10-14 00:50:19 +0300

    Update atm. lidar extinction operator to use the pre-computed optical properties file

--- observations/forward_operators/obs_def_atm_lidar_def_mod.f90
+++ observations/forward_operators/obs_def_atm_lidar_def_mod.f90
@@ -46,9 +46,10 @@ module obs_def_atm_lidar_mod
     VERTISHEIGHT, VERTISLEVEL, set_location_missing
   use     obs_kind_mod, only : QTY_ATM_LIDAR_EXTINCTION, QTY_DENSITY, &
     QTY_GC_DUST_BIN1, QTY_GC_DUST_BIN2, QTY_GC_DUST_BIN3, &
-    QTY_GC_DUST_BIN4, QTY_GC_DUST_BIN5
+    QTY_GC_DUST_BIN4, QTY_GC_DUST_BIN5, &
+    QTY_GC_SEAS_BIN1, QTY_GC_SEAS_BIN2, QTY_GC_SEAS_BIN3, &
+    QTY_GC_SEAS_BIN4
   use  assim_model_mod, only : interpolate
-
   use obs_def_utilities_mod, only : track_status
   use ensemble_manager_mod,  only : ensemble_type
 
@@ -68,13 +69,16 @@ module obs_def_atm_lidar_mod
   ! For loops
   integer :: i
 
-  type optical_properties
-    private
+  ! Represents a row in the optical properties CSV file
+  type :: optical_properties_csv
     integer :: wavelength ! Wavelength of the laser [nm]
-    integer :: size_bin ! Size bin number
-    real(r8) :: qext ! Extinction efficiency [unitless]
-    real(r8) :: rho ! Particle density [kg/m^3]
-    real(r8) :: effective_diameter ! Effective diameter [m]
+    character(len=6) :: species ! Species name
+    real(r8) :: ext_eff ! Extinction efficiency [meter ** 2 / kilogram]
+  end type optical_properties_csv
+
+  ! Represents the optical properties of an aerosol bin
+  type, extends(optical_properties_csv) :: optical_properties
+    integer :: qty ! DART quantity type
   end type optical_properties
 
   type(optical_properties), allocatable, dimension(:) :: optical_props
@@ -86,14 +90,15 @@ contains
   ! Read the optical properties from a CSV file
   subroutine initialize_module
     integer :: f, fstat ! File handle for optical properties
-    type(optical_properties) :: line
+    type(optical_properties_csv) :: line
+    type(optical_properties) :: props
 
     if (module_initialized) return
     call register_module(source, revision, revdate)
 
-    open(action='read', file='atm_lidar_optical_properties.csv', newunit=f, iostat=fstat)
+    open(action='read', file='GOCART_AOD_optical_properties.csv', newunit=f, iostat=fstat)
     if (fstat /= 0) then
-      write(string1, *) 'Could not open optical properties file atm_lidar_optical_properties.csv'
+      write(string1, *) 'Could not open optical properties file GOCART_AOD_optical_properties.csv'
       call error_handler(E_ERR, 'initialize_module', string1, source, revision, revdate)
     endif
 
@@ -106,21 +111,51 @@ contains
       read(f, *, iostat=fstat) line
       if (fstat /= 0) exit
 
-      ! Fix units
-      line%rho = line%rho * 1e6
-      line%effective_diameter = line%effective_diameter * 1e-6
+      ! Copy to props
+      props%wavelength = line%wavelength
+      props%species = line%species
+      props%ext_eff = line%ext_eff
 
-      optical_props = [optical_props, line]
+      ! Identify the quantity type
+      if (line%species == 'DUST_1') then
+        props%qty = QTY_GC_DUST_BIN1
+      else if (line%species == 'DUST_2') then
+        props%qty = QTY_GC_DUST_BIN2
+      else if (line%species == 'DUST_3') then
+        props%qty = QTY_GC_DUST_BIN3
+      else if (line%species == 'DUST_4') then
+        props%qty = QTY_GC_DUST_BIN4
+      else if (line%species == 'DUST_5') then
+        props%qty = QTY_GC_DUST_BIN5
+      else if (line%species == 'SEAS_1') then
+        props%qty = QTY_GC_SEAS_BIN1
+      else if (line%species == 'SEAS_2') then
+        props%qty = QTY_GC_SEAS_BIN2
+      else if (line%species == 'SEAS_3') then
+        props%qty = QTY_GC_SEAS_BIN3
+      else if (line%species == 'SEAS_4') then
+        props%qty = QTY_GC_SEAS_BIN4
+      else
+        write(*, *) 'Unknown species ignored: ', line%species
+        cycle
+      endif
+
+      optical_props = [optical_props, props]
     end do
+
+    print*, 'Read ', size(optical_props), ' optical properties'
+    if (size(optical_props) == 0) then
+      write(string1, *) 'No optical properties read from file'
+      call error_handler(E_ERR, 'initialize_module', string1, source, revision, revdate)
+    endif
 
     ! Print out the optical properties for debugging
     if (debug) then
       do i = 1, size(optical_props)
         write(*, *) 'Wavelength: ', optical_props(i)%wavelength
-        write(*, *) 'Size bin: ', optical_props(i)%size_bin
-        write(*, *) 'Qext: ', optical_props(i)%qext
-        write(*, *) 'Rho: ', optical_props(i)%rho
-        write(*, *) 'Effective diameter: ', optical_props(i)%effective_diameter
+        write(*, *) 'Species: ', optical_props(i)%species
+        write(*, *) 'DART quantity: ', optical_props(i)%qty
+        write(*, *) 'Ext_eff: ', optical_props(i)%ext_eff
       end do
     endif
 
@@ -129,19 +164,19 @@ contains
   end subroutine initialize_module
 
   ! Returns the optical properties for the given wavelength and size bin
-  subroutine get_optical_props(wavelength, bin, out)
+  subroutine get_optical_props(wavelength, qty, out)
     integer, intent(in) :: wavelength
-    integer, intent(in) :: bin
+    integer, intent(in) :: qty
     type(optical_properties), intent(out) :: out
 
     do i = 1, size(optical_props)
-      if (optical_props(i)%wavelength == wavelength .and. optical_props(i)%size_bin == bin) then
+      if (optical_props(i)%wavelength == wavelength .and. optical_props(i)%qty == qty) then
         out = optical_props(i)
         return
       endif
     end do
 
-    write(string1, *) 'Could not find optical properties for wavelength ', wavelength, ' and size bin ', bin
+    write(string1, *) 'Could not find optical properties for wavelength ', wavelength, ' and size bin ', qty
     call error_handler(E_ERR, 'get_optical_props', string1, source, revision, revdate)
   end subroutine get_optical_props
 
@@ -154,81 +189,56 @@ contains
     real(r8), intent(inout) :: extinction(ens_size)
     integer, intent(out) :: istatus(ens_size)
 
+    ! Optical property temp. storage
     type(optical_properties) :: props
-    real(r8) :: rho(ens_size), dust_1(ens_size), dust_2(ens_size), dust_3(ens_size), dust_4(ens_size), dust_5(ens_size)
+
+    ! For looping
+    integer :: obs_kind = 0
+
+    ! Which observation kinds to use for AOD
+    integer :: obs_kinds(9) = [QTY_GC_DUST_BIN1, QTY_GC_DUST_BIN2, QTY_GC_DUST_BIN3, QTY_GC_DUST_BIN4, QTY_GC_DUST_BIN5, &
+      QTY_GC_SEAS_BIN1, QTY_GC_SEAS_BIN2, QTY_GC_SEAS_BIN3, QTY_GC_SEAS_BIN4]
+
+    ! Storage for model state variables
+    real(r8) :: concentrations(ens_size, size(obs_kinds)), &
+      extinctions(ens_size, size(obs_kinds)), &
+      rho(ens_size)
+
+    ! Error handling
     integer :: this_istatus(ens_size)
     logical :: return_now = .false.
+
     real(r8) lon, lat, height, obsloc(3)
 
-    ! obsloc   = get_location(location)
-    ! lon      = obsloc(1)                       ! degree: 0 to 360
-    ! lat      = obsloc(2)                       ! degree: -90 to 90
-    ! height   = obsloc(3)                       ! (m)
-    ! ! write(*,*) 'Location: (x, y, z)', lon, lat, height
+    ! Initialise
+    call initialize_module()
+    concentrations(:, :) = MISSING_R8
+    extinctions(:, :) = MISSING_R8
+    rho(:) = MISSING_R8
 
-    ! Initialize arrays
-    rho = 0.0_r8
-    dust_1 = 0.0_r8
-    dust_2 = 0.0_r8
-    dust_3 = 0.0_r8
-    dust_4 = 0.0_r8
-    dust_5 = 0.0_r8
-    istatus = 0 ! All success
-    this_istatus = 0
-
-    call initialize_module
-
-    ! Grab air density (rho, [kg/m^3]) and dust mixing ratios from the state
+    ! Grab airdensity from state
     call interpolate(state_handle, ens_size, location, QTY_DENSITY, rho, this_istatus)
     call track_status(ens_size, this_istatus, extinction, istatus, return_now)
     if (return_now) return
 
-    call interpolate(state_handle, ens_size, location, QTY_GC_DUST_BIN1, dust_1, this_istatus)
-    call track_status(ens_size, this_istatus, extinction, istatus, return_now)
-    if (return_now) return
+    ! For each size bin, get mixing ratio, compute concentration and then extinction
+    do obs_kind = 1, size(obs_kinds)
+      call get_optical_props(355, obs_kinds(obs_kind), props)
 
-    call interpolate(state_handle, ens_size, location, QTY_GC_DUST_BIN2, dust_2, this_istatus)
-    call track_status(ens_size, this_istatus, extinction, istatus, return_now)
-    if (return_now) return
+      call interpolate(state_handle, ens_size, location, obs_kinds(obs_kind), &
+        concentrations(:, obs_kind), this_istatus)
+      call track_status(ens_size, this_istatus, extinction, istatus, return_now)
+      if (return_now) return
 
-    call interpolate(state_handle, ens_size, location, QTY_GC_DUST_BIN3, dust_3, this_istatus)
-    call track_status(ens_size, this_istatus, extinction, istatus, return_now)
-    if (return_now) return
-
-    call interpolate(state_handle, ens_size, location, QTY_GC_DUST_BIN4, dust_4, this_istatus)
-    call track_status(ens_size, this_istatus, extinction, istatus, return_now)
-    if (return_now) return
-
-    call interpolate(state_handle, ens_size, location, QTY_GC_DUST_BIN5, dust_5, this_istatus)
-    call track_status(ens_size, this_istatus, extinction, istatus, return_now)
-    if (return_now) return
-
-    ! Compute the extinction for each size bin
-    ! Extinction = (3 * Qext * MixingRatio * rho) / (2 * EffectiveDiameter * ParticleDensity)
-
-    call get_optical_props(355, 1, props)
-    where (istatus == 0) &
-      dust_1 = (3 * props%qext * dust_1 * rho * 1e-6) / (2 * props%effective_diameter * props%rho)
-
-    call get_optical_props(355, 2, props)
-    where (istatus == 0) &
-      dust_2 = (3 * props%qext * dust_2 * rho * 1e-6) / (2 * props%effective_diameter * props%rho)
-
-    call get_optical_props(355, 3, props)
-    where (istatus == 0) &
-      dust_3 = (3 * props%qext * dust_3 * rho * 1e-6) / (2 * props%effective_diameter * props%rho)
-
-    call get_optical_props(355, 4, props)
-    where (istatus == 0) &
-      dust_4 = (3 * props%qext * dust_4 * rho * 1e-6) / (2 * props%effective_diameter * props%rho)
-
-    call get_optical_props(355, 5, props)
-    where (istatus == 0) &
-      dust_5 = (3 * props%qext * dust_5 * rho * 1e-6) / (2 * props%effective_diameter * props%rho)
+      ! Multiply w/ density to get concentration
+      concentrations(:, obs_kind) = concentrations(:, obs_kind) * rho(:) * 1e-9  ! Convert from ug/m^3 to kg/m^3
+      extinctions(:, obs_kind) = concentrations(:, obs_kind) * props%ext_eff ! 1/m
+      extinctions(:, obs_kind) = max(extinctions(:, obs_kind), 0.0_r8) ! No negative extinctions
+    end do
 
     ! Sum the extinction from each size bin
     where (istatus == 0) &
-      extinction = dust_1 + dust_2 + dust_3 + dust_4 + dust_5
+      extinction = sum(extinctions, dim=2)
     where (istatus /= 0) &
       extinction = MISSING_R8
   end subroutine get_extinction
